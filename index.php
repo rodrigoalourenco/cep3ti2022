7bbb1639fbafbc80cbea3c89e99f8fbb0d1166c1
Rodrigo de Andrade Lourenço <rodrigoalourenco@hotmail.com>
2022-08-15 22:25:21 -0300

    Adicionando e alterando arquivo

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
-    echo "git pra vida!!!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco de Dados</title>
+</head>
+<body>
+    <form action="processa.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    
+</body>
+</html>
